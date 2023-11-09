c2be1c311ea6a42560ec5a78962435bebae4bce6
Aakash <99542364+Aakash-kushwaha@users.noreply.github.com>
2023-11-09 13:32:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
-const connection=mongoose.connect(process.env.mongourl)
+const connection=mongoose.connect('mongourl=mongodb+srv://chauhanrohit716:Rohit221@cluster0.yhazr7e.mongodb.net/wordcount?retryWrites=true&w=majority')
 
 module.exports={
     connection
